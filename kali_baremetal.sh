587095e01d439c9e5049ab877d50f01e330029f4
Paul Clark <paulgclark@users.noreply.github.com>
2024-10-15 23:23:07 -0700

    added kali install script

---
+++ kali_baremetal.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+# update system and install prereqs
+sudo apt update
+sudo apt -y upgrade
+sudo apt install -y libuhd-dev uhd-host
+sudo cp /usr/libexec/uhd/utils/uhd-usrp.rules /etc/udev/rules.d/.
+
+# install urh
+pipx install urh
+pipx ensurepath
+
+# fix numpy issue
+pipx runpip urh install numpy==1.26.4
+pipx runpip urh install setuptools
+
+echo "Installation complete. Please reboot before continuing..."
