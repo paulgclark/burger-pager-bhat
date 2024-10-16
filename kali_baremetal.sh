8e357eef024c935362d0b3a5df7ff9a0ccc11068
Paul Clark <paulgclark@gmail.com>
2024-10-16 16:14:19 -0700

    added bare metal install and refresh scripts

--- kali_baremetal.sh
+++ kali_baremetal.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/bash
 
-# update system and install prereqs
+# install prereqs
 sudo apt update
-sudo apt -y upgrade
-sudo apt install -y libuhd-dev uhd-host
+#sudo apt -y upgrade
+sudo apt install -y pipx libuhd-dev uhd-host
+# allow access to usrp in user mode
 sudo cp /usr/libexec/uhd/utils/uhd-usrp.rules /etc/udev/rules.d/.
 sudo udevadm control --reload-rules
+sudo udevadm trigger
+# get fpga images for b200
+uhd_images_downloader -t b2xx_b200_fpga_default b2xx_b200mini_fpga_default b2xx_common_fw_default
 
 # install urh
 pipx install urh
@@ -15,4 +19,3 @@ pipx ensurepath
 pipx runpip urh install numpy==1.26.4
 pipx runpip urh install setuptools
 
-echo "Installation complete. Please reboot before continuing..."
