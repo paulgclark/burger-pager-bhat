9a333881906720e90ef98076a5048829654532b6
Paul Clark <paulgclark@users.noreply.github.com>
2024-10-22 21:15:17 -0400

    fixed images download bug

--- kali_baremetal.sh
+++ kali_baremetal.sh
@@ -9,7 +9,7 @@ sudo cp /usr/libexec/uhd/utils/uhd-usrp.rules /etc/udev/rules.d/.
 sudo udevadm control --reload-rules
 sudo udevadm trigger
 # get fpga images for b200
-uhd_images_downloader -t b2xx_b200_fpga_default b2xx_b200mini_fpga_default b2xx_common_fw_default
+sudo uhd_images_downloader 
 
 # install urh
 pipx install urh
@@ -19,3 +19,4 @@ pipx ensurepath
 pipx runpip urh install numpy==1.26.4
 pipx runpip urh install setuptools
 
+echo "Install complete. Reboot and remember to rebuild your drivers in URH"
